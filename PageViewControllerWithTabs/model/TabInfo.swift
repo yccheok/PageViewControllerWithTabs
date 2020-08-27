4a9339a2ca5c2b63290242c6129008bfd29e806e
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2020-08-27 23:29:17 +0800

    Introduce TabInfo

---
+++ PageViewControllerWithTabs/model/TabInfo.swift
@@ -0,0 +1,9 @@
+//
+//  TabInfo.swift
+//  PageViewControllerWithTabs
+//
+//  Created by guest2 on 27/08/2020.
+//  Copyright © 2020 Leela Prasad. All rights reserved.
+//
+
+import Foundation
---
+++ PageViewControllerWithTabs/ui/Utils+UI.swift
@@ -0,0 +1,14 @@
+//
+//  Utils.swift
+//  PageViewControllerWithTabs
+//
+//  Created by guest2 on 27/08/2020.
+//  Copyright © 2020 Leela Prasad. All rights reserved.
+//
+
+import Foundation
+
+class UIUtils {
+    private init() {        
+    }
+}
